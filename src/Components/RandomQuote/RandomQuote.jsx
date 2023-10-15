f929b2f33c4b016875f283ba7896776fca4d8602
Amasias-Rodriguez <rodamasias01@gmail.com>
2023-10-15 02:20:52 -0600

    modifications

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -4,19 +4,34 @@ import twitter_icon from '../Assets/twitter.png'
 import reload_icon from '../Assets/reload.png'
 const RandomQuote = () => {
 
+    let quotes = [];
+
+    async function loadQuotes() {
+        const response = await fetch("https://type.fit/api/quotes");
+        quotes = await response.json();
+    }
+
     const [quote, setQuote] = useState({
         text: "Difficulties increase the nearer we get to the goal.",
         author: "Johann Wolfgang von Goethe",
-    })
+    });
+
+    const random = () => {
+        const select = quotes[Math.floor(Math.random() * quotes.length)];
+        setQuote(select);
+    }
+
+    loadQuotes();
     return (
-        <div className='Container'>
+        <div className='container'>
             <div className="quote">{quote.text}</div>
             <div>
                 <div className="line"></div>
                 <div className="bottom">
-                    <div className="author">{quote.author}</div>
+                    <div className="author">{quote.author.split(',')[0]}</div>
                     <div className="icons">
-                        <img src={reload_icon} alt="reload-icon" />
+                        <img src={reload_icon} onClick={() => { random() }}
+                            alt="reload-icon" />
                         <img src={twitter_icon} alt="twitter-icon" />
                     </div>
                 </div>
